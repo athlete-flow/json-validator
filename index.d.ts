931a45d7fcc92857684de476caf4e66d95b245b0
addnsk <kyle_prod@mail.ru>
2025-05-22 04:38:39 +0200

    fix typing

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,24 @@
 type Depth = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export type NeverSymbol = Record<symbol, never>;
+type NeverSymbol = Record<symbol, never>;
 
-export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
 
-export type LastOf<T> = UnionToIntersection<T extends any ? (x: T) => void : never> extends (x: infer L) => void
-  ? L
-  : never;
+type LastOf<T> = UnionToIntersection<T extends any ? (x: T) => void : never> extends (x: infer L) => void ? L : never;
 
-export type UnionToTuple<T, R extends any[] = []> = [T] extends [never]
+type UnionToTuple<T, R extends any[] = []> = [T] extends [never]
   ? R
   : UnionToTuple<Exclude<T, LastOf<T>>, [LastOf<T>, ...R]>;
 
 type Tokens = StringConstructor | NumberConstructor | BooleanConstructor | null | undefined;
-export type TokenDefinition = Tokens | AbstractShape | (TokenDefinition | AbstractShape)[];
+
+type TokenDefinition = Tokens | AbstractShape | (TokenDefinition | AbstractShape)[];
 
 export type AbstractShape = {
   [key: string]: TokenDefinition;
 };
 
-export type InferToken<T, D extends number> = [D] extends [0]
+type InferToken<T, D extends number> = [D] extends [0]
   ? never
   : T extends string
   ? StringConstructor
@@ -30,26 +29,30 @@ export type InferToken<T, D extends number> = [D] extends [0]
   : T extends null | undefined
   ? T
   : T extends (infer U)[]
-  ? UnionToTuple<InferToken<U, Depth[D]>>
+  ? Readonly<UnionToTuple<InferToken<U, Depth[D]>>>
   : T extends Record<string, any>
-  ? Shape<T, Depth[D]>
+  ? ShapeWithDepth<T, Depth[D]>
   : never;
 
-export type Shape<T extends Entity, D extends number = 10> = {
-  [K in keyof T]-?: UnionToTuple<InferToken<T[K], D>>;
+type ShapeWithDepth<T extends Entity, D extends number = 10> = {
+  [K in keyof T]-?: Readonly<UnionToTuple<InferToken<T[K], D>>>;
+};
+
+export type Shape<T extends Entity> = {
+  [K in keyof T]-?: Readonly<UnionToTuple<InferToken<T[K], 10>>>;
 };
 
 type Types = string | number | boolean | null | undefined;
 
-export type JSONValue = Types | JSONArray | Entity;
+type JSONValue = Types | JSONArray | Entity;
 
-export type JSONArray = JSONValue[];
+type JSONArray = JSONValue[];
 
 export type Entity = NeverSymbol & {
   [key: string]: JSONValue;
 };
 
-export type InferType<T, D extends number> = [D] extends [0]
+type InferType<T, D extends number> = [D] extends [0]
   ? never
   : T extends StringConstructor
   ? string
@@ -62,11 +65,15 @@ export type InferType<T, D extends number> = [D] extends [0]
   : T extends Array<infer U>
   ? InferType<U, Depth[D]>[]
   : T extends AbstractShape
-  ? InferEntity<T, Depth[D]>
+  ? InferEntityWithDepth<T, Depth[D]>
   : never;
 
-export type InferEntity<T extends AbstractShape, D extends number = 10> = {
+export type InferEntityWithDepth<T extends AbstractShape, D extends number = 10> = {
   [K in keyof T]: T[K] extends (infer U)[] ? InferType<U, D> : never;
+};
+
+export type InferEntity<T extends AbstractShape> = {
+  [K in keyof T]: T[K] extends (infer U)[] ? InferType<U, 10> : never;
 };
 
 export type ParseResult<T extends Entity> =
@@ -79,6 +86,7 @@ export interface ISchema<T extends Entity> {
 }
 
 export declare class SchemaFactory {
-  createSchema<T extends AbstractShape>(shape: T): ISchema<InferEntity<T>>;
-  createSchema<T extends Entity>(shape: Shape<T>): ISchema<T>;
+  createSchema<T extends Entity | AbstractShape>(
+    shape: T extends Entity ? Shape<T> : T
+  ): T extends AbstractShape ? ISchema<InferEntity<T>> : T extends Entity ? ISchema<T> : never;
 }
